95d257dbd90de2c813804c6537a6c98ebf90a1d7
Caio Mafra <gamersb200@gmail.com>
2023-03-21 17:40:59 -0300

    Remove

--- script.js
+++
@@ -1,7 +0,0 @@
-let isIgnite = false
-function changeCard(event) {
-  const card = event.currentTarget
-  const bg = isIgnite ? "explorer" : "ignite"
-  isIgnite = !isIgnite
-  card.style.backgroundImage = `url(./assets/bg-${bg}.svg)`
-}
